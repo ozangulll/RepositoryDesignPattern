acd47d9ad996c8614e12feec1d00253db2877f45
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-02-18 16:04:14 +0300

    Product Data Access Layer visibility level changed as public to configure services in program.cs

--- RepositoryDesignPattern.DataAccessLayer/EntityFramework/EfProductDal.cs
+++ RepositoryDesignPattern.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace RepositoryDesignPattern.DataAccessLayer.EntityFramework
 {
-    internal class EfProductDal : GenericRepository<Product>, IProductDal
+    public class EfProductDal : GenericRepository<Product>, IProductDal
     {
         public EfProductDal(Context context) : base(context)
         {
