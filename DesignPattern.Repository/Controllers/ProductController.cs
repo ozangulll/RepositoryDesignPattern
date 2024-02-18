7e46a0958b7a0300767381c4ed227bf3488bfca3
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-02-18 15:58:50 +0300

    controller for product added

---
+++ DesignPattern.Repository/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryDesignPattern.BusinessLayer.Abstract;
+using RepositoryDesignPattern.EntityLayer.Concrete;
+
+namespace RepositoryDesignPattern.PresentationLayer.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _productService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _productService.TInsert(product);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _productService.TGetByID(id);
+            _productService.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = _productService.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
