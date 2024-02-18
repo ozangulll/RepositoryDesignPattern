de94da36fd48de7f712ec2a65eb5dd4ee82e54c3
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-02-18 14:45:04 +0300

    methods added for generic

---
+++ RepositoryDesignPattern.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using RepositoryDesignPattern.DataAccessLayer.Abstract;
+using RepositoryDesignPattern.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryDesignPattern.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
