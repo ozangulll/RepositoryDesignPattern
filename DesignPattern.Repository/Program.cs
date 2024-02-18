4f0672e90c1c97d48c8422993cf790ebfd539b9c
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-02-18 16:04:33 +0300

    Configuring services for product

--- DesignPattern.Repository/Program.cs
+++ DesignPattern.Repository/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<Context>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<IProductDal, EfProductDal>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
